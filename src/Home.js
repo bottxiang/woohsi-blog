69635deb421ca783322e57aabe8bfdef8f812619
wsl-dell <bottxiang@gmail.com>
2022-01-12 17:35:43 +0800

    update

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
+import React, { useState } from 'react';
+
 const Home = () => {
+  const [name, setName] = useState("A")
 
   const handleClick = (e) => {
-     console.log(e)
+    setName("B")
+    console.log(e)
   };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={() => { handleClick("param")}}>Yes</button>
     </div>
   );
